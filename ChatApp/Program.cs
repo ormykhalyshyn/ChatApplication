20bc7920f0ee4cdfae8aee624c5a050ddc9b93ac
morest261 <or.mykhalyshyn@gmail.com>
2023-01-27 00:27:02 +0200

    Add project files.

---
+++ ChatApp/Program.cs
@@ -0,0 +1,32 @@
+using ChatApp.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
